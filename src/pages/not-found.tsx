3721d5b90eb10a3eaa8fef9170e74ef24d3e67cc
wei.shi <weishi258@gmail.com>
2019-03-22 01:25:31 -0400

    modify not-found page to get rid of row/col

--- src/pages/not-found.tsx
+++ src/pages/not-found.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { Button, Col, Row } from 'antd';
+import { Button } from 'antd';
 import { FormattedMessage } from 'react-intl';
 import './not-found.less';
 import { RouteComponentProps } from 'react-router';
@@ -12,27 +12,21 @@ class NotFound extends React.Component<RouteComponentProps> {
     public render() {
         return (
             <div className="layout-top-layer not-found">
-                <Row
-                    type="flex"
-                    justify="center"
-                    align="middle"
-                    className="container"
-                >
-                    <Col span={24}>
-                        <h1>404</h1>
-                        <div className="not-found-desc">
-                            <FormattedMessage id="not_fount_desc" />
-                        </div>
-                        <div className="not-fount-actions">
-                            <Button
-                                type="primary"
-                                onClick={this.onClickBackToHome}
-                            >
-                                <FormattedMessage id="back_to_home" />
-                            </Button>
-                        </div>
-                    </Col>
-                </Row>
+                <div className="container">
+                    <h1>404</h1>
+                    <div className="not-found-desc">
+                        <FormattedMessage id="not_fount_desc" />
+                    </div>
+                    <div className="not-fount-actions">
+                        <Button
+                            type="primary"
+                            size="large"
+                            onClick={this.onClickBackToHome}
+                        >
+                            <FormattedMessage id="back_to_home" />
+                        </Button>
+                    </div>
+                </div>
             </div>
         );
     }
